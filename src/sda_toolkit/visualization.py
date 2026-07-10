4699703da546dfb4cd3fb75308e438167c898555
ditno <workpriyanshu23@gmail.com>
2026-07-10 23:16:37 +0530

    code fixes

--- src/sda_toolkit/visualization.py
+++ src/sda_toolkit/visualization.py
@@ -29,7 +29,7 @@ import matplotlib.pyplot as plt
 
 def bar_chart(df,x,y, save_path = None):
     fig, ax = plt.subplots()
-    ax.plot(kind='bar')
+    ax.bar(df[x],df[y])
     ax.set_xlabel(x)
     ax.set_ylabel(y)
     ax.set_title(f'{x} vs {y}')
@@ -42,7 +42,7 @@ def bar_chart(df,x,y, save_path = None):
 
 def line_chart(df,x,y,save_path = None):
     fig, ax = plt.subplots()
-    ax.plot(df[f], df[y])
+    ax.plot(df[x], df[y])
     ax.set_xlabel(x)
     ax.set_ylabel(y)
     ax.set_title(f'{x} vs {y}')
@@ -54,19 +54,19 @@ def line_chart(df,x,y,save_path = None):
     plt.close()
 
 def histogram(df,column , bins=20, save_path = None):
-     fig, ax = plt.subplots()
-     ax.hist(df[column], bins = bins)
-     ax.set_xlabel(column)
-     ax.set_ylabel('Frequecy')
-     ax.set_title(f'distributin of {column}')
-     plt.tight_layout()
+    fig, ax = plt.subplots()
+    ax.hist(df[column], bins = bins)
+    ax.set_xlabel(column)
+    ax.set_ylabel('Frequecy')
+    ax.set_title(f'distributin of {column}')
+    plt.tight_layout()
     if save_path:
         plt.savefig(save_path)
     else:
         plt.show()
     plt.close()
 
-def Scatter_plot(df,x,y, save_path = None):
+def scatter_plot(df,x,y, save_path = None):
     fig, ax = plt.subplots()
     ax.scatter(df[x],df[y])
     ax.set_xlabel(x)
